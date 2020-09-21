f61069e165766c311a5e1bb172f687cc3f2ce652
AlexFleischerParis <scoralex@yahoo.com>
2020-09-21 12:28:11 +0200

    Create zoogetcplexinfo.py

---
+++ zoogetcplexinfo.py
@@ -0,0 +1,14 @@
+from docplex.mp.model import Model
+
+mdl = Model(name='buses')
+nbbus40 = mdl.integer_var(name='nbBus40')
+nbbus30 = mdl.integer_var(name='nbBus30')
+mdl.add_constraint(nbbus40*40 + nbbus30*30 >= 300, 'kids')
+mdl.minimize(nbbus40*500 + nbbus30*400)
+
+mdl.solve()
+
+for v in mdl.iter_integer_vars():
+    print(v," = ",v.solution_value)
+
+print("time = ",mdl.solve_details.time)
