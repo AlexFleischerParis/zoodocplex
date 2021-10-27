05111d04677212d98dc5b30d9a2434693e822c30
AlexFleischerParis <scoralex@yahoo.com>
2021-10-27 14:13:48 +0200

    Create zoolazy.py

---
+++ zoolazy.py
@@ -0,0 +1,23 @@
+from docplex.mp.model import Model
+
+mdl = Model(name='buses')
+nbbus40 = mdl.integer_var(name='nbBus40')
+nbbus30 = mdl.integer_var(name='nbBus30')
+mdl.add_constraint(nbbus40*40 + nbbus30*30 >= 300, 'kids')
+mdl.minimize(nbbus40*500 + nbbus30*400)
+
+mdl.add_lazy_constraint(nbbus40<=5)
+
+mdl.solve(log_output=True,)
+
+for v in mdl.iter_integer_vars():
+    print(v," = ",v.solution_value)
+
+"""
+
+which gives
+
+nbBus40  =  3.0
+nbBus30  =  6.0
+
+"""
