5157efe20f24108417326bda428e6b8aef5c3502
AlexFleischerParis <scoralex@yahoo.com>
2021-11-12 08:42:30 +0100

    Create zoowritesolutioninafile.py

---
+++ zoowritesolutioninafile.py
@@ -0,0 +1,29 @@
+from docplex.mp.model import Model
+
+mdl = Model(name='buses')
+nbbus40 = mdl.integer_var(name='nbBus40')
+nbbus30 = mdl.integer_var(name='nbBus30')
+mdl.add_constraint(nbbus40*40 + nbbus30*30 >= 300, 'kids')
+mdl.minimize(nbbus40*500 + nbbus30*400)
+mdl.solve(log_output=True,)
+
+#display solution
+for v in mdl.iter_integer_vars():
+    print(v," = ",v.solution_value)
+
+#write solution to a file
+f= open("c://temp//sol.txt", "w")
+for v in mdl.iter_integer_vars():
+    f.write(str(v)+" = "+str(v.solution_value)+'\n')
+f.close()
+
+"""
+
+which gives
+
+nbBus40  =  6.0
+nbBus30  =  2.0
+
+in the display and in the file
+
+"""
