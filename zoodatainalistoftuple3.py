182edbed75e9163823bdf72594d55ba187f0358b
AlexFleischerParis <scoralex@yahoo.com>
2022-04-13 17:39:22 +0200

    Update zoodatainalistoftuple3.py

--- zoodatainalistoftuple3.py
+++ zoodatainalistoftuple3.py
@@ -1,8 +1,4 @@
-
-
-from docplex.cp.model import CpoModel
-
-# Data
+from docplex.mp.model import Model 
 
 Buses=[
     (40,500),
@@ -20,7 +16,7 @@ busCost=1;
 for b in Buses:
     print("buses with ",b[busSize]," seats cost ",b[busCost])
 
-mdl = CpoModel(name='buses')
+mdl = Model(name='buses')
 
 #decision variables
 
@@ -28,13 +24,25 @@ mdl.nbBus = mdl.integer_var_list(nbSizes,0,1000,name="nbBus")
 
 
 # Constraint
-mdl.add(mdl.sum(mdl.nbBus[b]*Buses[b][busSize] for b in range(0,nbSizes)) >= nbKids)
+mdl.add(sum(mdl.nbBus[b]*Buses[b][busSize] for b in range(0,nbSizes)) >= nbKids)
 
 # Objective
-mdl.minimize(mdl.sum(mdl.nbBus[b]*Buses[b][busCost] for b in range(0,nbSizes)))
+mdl.minimize(sum(mdl.nbBus[b]*Buses[b][busCost] for b in range(0,nbSizes)))
 
 msol=mdl.solve()
 
 # Dislay solution
 for b  in range(0,nbSizes):
-    print(msol[mdl.nbBus[b]]," buses with ",Buses[b][busSize]," seats"); 
+    print(msol[mdl.nbBus[b]]," buses with ",Buses[b][busSize]," seats")
+
+"""
+
+gives
+
+buses with  40  seats cost  500
+buses with  30  seats cost  400
+6.0  buses with  40  seats
+2.0  buses with  30  seats
+
+"""
+
