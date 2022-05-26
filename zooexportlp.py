e10acd9145e4155a618d7d4b9278124e84717167
AlexFleischerParis <scoralex@yahoo.com>
2022-05-26 11:07:10 +0200

    zooexportlp

--- zooexportlp.py
+++ zooexportlp.py
@@ -6,17 +6,29 @@ nbbus30 = mdl.integer_var(name='nbBus30')
 mdl.add_constraint(nbbus40*40 + nbbus30*30 >= 300, 'kids')
 mdl.minimize(nbbus40*500 + nbbus30*400)
 
-mdl.solve(log_output=True,)
 
-for v in mdl.iter_integer_vars():
-    print(v," = ",v.solution_value)
+
+mdl.export("c:\\temp\\buses.lp")
+
+
 
 
 """
 
-which gives
+which gives in buses.lp
 
-nbBus40  =  6.0
-nbBus30  =  2.0
+Minimize
+ obj: 500 nbBus40 + 400 nbBus30
+Subject To
+ kids: 40 nbBus40 + 30 nbBus30 >= 300
+
+Bounds
+
+Generals
+ nbBus40 nbBus30
+End
+
+
+
 
 """
